0fba341ea00355ac60cb0e8fff49d7bfaafc36f8
GONRONDEROS <gonzalorond@gmail.com>
2025-09-20 18:55:23 -0300

    ejercicio 01

--- Modulo-02/src/Ejercicio01/Ejercicio01.java
+++ Modulo-02/src/Ejercicio01/Ejercicio01.java
@@ -2,7 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
 package Ejercicio01;
+
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +17,17 @@ public class Ejercicio01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese un año: ");
+        int ano = scanner.nextInt();
+        
+        if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+            System.out.println("El año " + ano + " es bisiesto");
+        } else {
+            System.out.println("El año " + ano + " no es bisiesto");
+        }
+        scanner.close();
     }
     
 }
